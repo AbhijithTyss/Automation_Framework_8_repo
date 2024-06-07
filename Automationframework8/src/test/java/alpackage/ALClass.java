4bcec3ebf1a74b9431caf3b7c158b6e1e26a8226
AbhijithTyss <abhijith.styss@gmail.com>
2024-06-07 09:38:52 +0530

    ALClass added

---
+++ Automationframework8/src/test/java/alpackage/ALClass.java
@@ -0,0 +1,11 @@
+package alpackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ALClass {
+	@Test
+	public void demo() {
+		Reporter.log("ALClass added",true);
+	}
+}
