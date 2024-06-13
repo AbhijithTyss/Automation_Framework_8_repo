568dceefb7d050e9c70be8878185f78dff30ef42
AbhijithTyss <abhijith.styss@gmail.com>
2024-06-13 08:57:50 +0530

    TC_002_Test class added

---
+++ Automationframework8/src/test/java/login/TC_002_Test.java
@@ -0,0 +1,11 @@
+package login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_002_Test {
+	@Test
+	public void demo() {
+		Reporter.log("TC_002_Test class added",true);
+	}
+}
